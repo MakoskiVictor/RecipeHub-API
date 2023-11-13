0d30defe6e1f7930de8e9ddadf36a08fe4018456
MakoskiVictor <makoski.ed@gmail.com>
2023-11-13 20:06:42 -0300

    import recipe and diet

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RecipeModule } from './recipe';
+import { DietModule } from './diet';
 
 @Module({
-  imports: [],
+  imports: [RecipeModule, DietModule],
   controllers: [AppController],
   providers: [AppService],
 })
