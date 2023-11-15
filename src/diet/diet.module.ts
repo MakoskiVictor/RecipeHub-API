7469cd7b32b2b04d2e1e471edf04b1054ad756e1
MakoskiVictor <makoski.ed@gmail.com>
2023-11-15 20:47:06 -0300

    import Diet Schema

--- src/diet/diet.module.ts
+++ src/diet/diet.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DietController } from './diet.controller';
 import { DietService } from './diet.service';
+import { Diet } from 'src/entities/diet.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+// Debo avisar al módulo, que entidad voy a estar usando
 @Module({
+  imports: [TypeOrmModule.forFeature([Diet])],
   controllers: [DietController],
   providers: [DietService],
 })
