3fe451955a12e356c43321a3c34bb4d2d210ce04
MakoskiVictor <makoski.ed@gmail.com>
2023-11-15 20:46:28 -0300

    configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecipeModule } from './recipe';
 import { DietModule } from './diet';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Diet } from './entities/diet.entity';
+import { Recipe } from './entities/recipe.entity';
 
 @Module({
-  imports: [RecipeModule, DietModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'recipehub',
+      entities: [Diet, Recipe],
+      synchronize: true,
+    }),
+    RecipeModule,
+    DietModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
