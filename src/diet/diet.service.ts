fdcfb17d60a33df649453ea56a4f0b5d59f7b792
MakoskiVictor <makoski.ed@gmail.com>
2023-11-15 20:47:33 -0300

    add get and post diets

--- src/diet/diet.service.ts
+++ src/diet/diet.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Diet } from 'src/entities/diet.entity';
+import { Repository } from 'typeorm';
+import { CreateDietDTO } from './dto/create-diet.dto';
 
 @Injectable()
 export class DietService {
+  // Se inyecta la db con el constructor
+  constructor(
+    @InjectRepository(Diet) private dietRepository: Repository<Diet>,
+  ) {}
+
   // Se crean los métodos a inyectar en el controller
-  getDiets() {
-    return 'These are all the diets!';
+  postDiet(diet: CreateDietDTO) {
+    const newDiet = this.dietRepository.create(diet);
+    return this.dietRepository.save(newDiet);
+  }
+  async getDiets() {
+    return this.dietRepository.find();
   }
 }
