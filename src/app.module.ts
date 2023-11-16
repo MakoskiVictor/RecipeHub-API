75ab9af37a73d41a8306661142a9f3f1f87ac07f
MakoskiVictor <makoski.ed@gmail.com>
2023-11-15 21:50:13 -0300

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,21 @@ import { DietModule } from './diet';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Diet } from './entities/diet.entity';
 import { Recipe } from './entities/recipe.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'recipehub',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Diet, Recipe],
       synchronize: true,
     }),
