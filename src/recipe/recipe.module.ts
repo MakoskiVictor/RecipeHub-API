c8f4942a0fd79c956463c0fdbd58a40f6cc5b48f
MakoskiVictor <makoski.ed@gmail.com>
2023-11-18 12:03:40 -0300

    import Diet entity

--- src/recipe/recipe.module.ts
+++ src/recipe/recipe.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Diet } from 'src/entities/diet.entity';
+import { Recipe } from 'src/entities/recipe.entity';
 
+// Importo las tablas a usar en este módulo
 @Module({
+  imports: [TypeOrmModule.forFeature([Diet, Recipe])],
   controllers: [RecipeController],
   providers: [RecipeService],
 })
