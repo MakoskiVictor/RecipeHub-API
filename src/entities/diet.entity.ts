6a7168199a729172ff0929f08644df8ee10b956f
MakoskiVictor <makoski.ed@gmail.com>
2023-11-18 12:03:13 -0300

    change id from number to string

--- src/entities/diet.entity.ts
+++ src/entities/diet.entity.ts
@@ -3,8 +3,8 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Diet {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
