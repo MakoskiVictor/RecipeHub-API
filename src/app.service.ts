05dd70e1249303b5ee8ad7c00627fe508c70acb1
MakoskiVictor <makoski.ed@gmail.com>
2023-11-23 15:54:48 -0300

    add routes explanation

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,40 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object[] {
+    return [
+      {
+        route: '/diet',
+        method: 'Get',
+        explanation: 'Get list of diets',
+      },
+      {
+        route: '/recipe',
+        method: 'Get',
+        explanation: 'Get list of recipes',
+      },
+      {
+        route: '/recipe',
+        method: 'Post',
+        explanation: 'Post a new recipe',
+        structure: `
+        name: string,
+        summary: string,
+        healthScore: number(0 to 100),
+        steps: string,
+        image: url,
+        diets: Diet[dietName]`,
+      },
+      {
+        route: '/recipe/:id',
+        method: 'Get',
+        explanation: 'Get an especific recipe',
+      },
+      {
+        route: '/recipe/:id',
+        method: 'Delete',
+        explanation: 'Delete an especific recipe',
+      },
+    ];
   }
 }
